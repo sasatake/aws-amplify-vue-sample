099ca0e0028c932c0977bc5881045afb2f03040a
sasatake <t06418as@gmail.com>
2021-02-07 16:29:59 +0900

    remove HelloWorld Component add List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container>
+    <b-jumbotron
+      header="Amplify Vue Sample"
+      header-level="4"
+      lead="with vue.js"
+      bg-variant="light"
+      class="text-center"
+    >
+    </b-jumbotron>
+
+    <b-row align-h="center">
+      <b-col cols="6">
+        <b-table striped hover :items="items"></b-table>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data() {
+    return {
+      items: [
+        {id: 1,task: "牛乳を買う", expired_date: "2021-02-10"}
+      ],
+    };
+  },
+}
+</script>
+
+<style scoped></style>
