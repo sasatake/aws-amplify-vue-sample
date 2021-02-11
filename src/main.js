ac624cee2375c5c3ff4bd72f28682775c55471d4
sasatake <t06418as@gmail.com>
2021-02-12 07:26:32 +0900

    add vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import './plugins/bootstrap-vue'
 import './plugins/aws-amplify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
